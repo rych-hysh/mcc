2005a9e0e784648306475ba36defc4642481bf27
hrhrknight <hayaryo99@gmail.com>
2023-07-04 19:06:55 +0900

    implements add and sub

--- msc.cpp
+++ msc.cpp
@@ -8,10 +8,32 @@ int main(int argc, char **argv){
     fprintf(stderr, "invalid number of args");
     return 1;
   }
+
+  //プログラムを１文字ずつ読み込む
+  char *p = argv[1];
+
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  //strtolは数値を読み込んだ後、第二引数のポインタをアップデートし読み込んだ最後の文字の次の文字を指すように値を更新する
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+  while(*p){
+    if(*p == '+'){
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+    if(*p == '-'){
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+    fprintf(stderr, "unexpected character detected: '%c'\n", *p);
+    return 1;
+  }
+
   printf("  ret\n");
+
   return 0;
 }
